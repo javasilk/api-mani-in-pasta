b27efb426b7c107e45e6f2a29d4951a86a60ac89
andrea <asalidu@it.tiscali.com>
2015-06-18 09:54:50 +0200

    aggiunto test user create

--- src/CloudSkill/Bundle/TestBundle/Controller/DefaultController.php
+++ src/CloudSkill/Bundle/TestBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace CloudSkill\Bundle\TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CloudSkill\Bundle\ManiInPastaBundle\Entity\ManiInPasta\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -10,4 +13,38 @@ class DefaultController extends Controller
     {
         return $this->render('CloudSkillTestBundle:Default:index.html.twig', array('name' => $name));
     }
+    
+    
+    public function userCreateAction(Request $request)
+    {
+        $username = $request->request->get('user');
+        $pass = $request->request->get('password');
+        $nome = $request->request->get('nome');
+        $cognome = $request->request->get('cognome');
+        $status = $request->request->get('status');
+        $token = $request->request->get('token');
+        
+        
+        $user = new CloudSkill\Bundle\ManiInPastaBundle\Entity\ManiInPasta\User();
+        $user->setNome($nome);
+        $user->setCognome($cognome);
+        $user->setUser($username);
+        $user->setStatus($status);
+        $user->setPassword($pass);
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($user);
+        $em->flush();
+        $array = array("id"=>$user->getId());
+        return new Response(json_encode($array));        
+        
+    }
+    
 }
+
+
+
+
+
+
+
