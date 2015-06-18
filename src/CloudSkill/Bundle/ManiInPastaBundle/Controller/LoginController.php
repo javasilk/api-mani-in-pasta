daa48058e31dbf5e9a7c9f3bbecd3a1d01732df4
andrea <asalidu@it.tiscali.com>
2015-06-18 16:34:02 +0200

    rimosso codice

--- src/CloudSkill/Bundle/ManiInPastaBundle/Controller/LoginController.php
+++ src/CloudSkill/Bundle/ManiInPastaBundle/Controller/LoginController.php
@@ -11,72 +11,14 @@ use Symfony\Component\Serializer\Exception\Exception;
 class LoginController extends Controller {
 
     public function loginAction(Request $request, $_format) {
-        $username = $request->request->get('username');
-        $pass = $request->request->get('password');
-        if (empty($username)) {
-            throw new HttpException(400, "Bad User");
-        }
-
-        if (empty($pass)) {
-            throw new HttpException(400, "Bad pass");
-        }
-
-        try {
-            $userRepo = $this->getDoctrine()->getRepository('CloudSkillManiInPastaBundle:User');
-            /* @var $user User */
-            $user = $userRepo->findOneBy(array('username' => $username, 'password'=>sha1($pass)));
-
-            if ($user == null) {
-                throw new HttpException(403, "Forbidden");
-            }
-            if (!$user->getStatus() or $user->getStatus() != "active") {
-                throw new HttpException(403, "Forbidden: user not active");
-            }
-
+        try{
             $response = new Response();
-            $user->toArray();
-            $response->setContent(json_encode($user->toArray()));
+            $response->setContent(json_encode(array()));
             $response->headers->set('Content-Type', 'application/json');
             return $response;
         } catch (Exception $e) {
             throw new HttpException(500, "Generic server errror: " . $e->getMessage());
         }
-//        if(empty($user)){
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(400,"Bad User");
-//        }
-//        
-//        if(empty($pass)){
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(400,"Bad pass");
-//        }
-//        
-//        if($user == "disable"  and $pass=="letmein"){
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(403,"Forbidden User Disable");
-//        }        
-//        
-//        if($user == "inactive"  and $pass=="letmein"){
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(403,"Forbidden User Disable");
-//        }    
-//        
-//        if($user == "test"  and $pass=="sbagliata"){
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(403,"User not found");
-//        }    
-//        if($user == "cippalippa"  and $pass=="sbagliata"){
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(403,"User not found");
-//        }    
-//        
-//        
-//        
-//        
-//        if($user == "test"  and $pass=="letmein"){
-//            $response = new Response();
-//            $resultArray = array("username"=>$username,"userid"=>"123","fullname"=>"utente test");
-//            $response->setContent(json_encode($resultArray));
-//            $response->headers->set('Content-Type', 'application/json');
-//            return $response;
-//            
-//        }else {
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(404,"User Not Found ".$user.": ".$pass);
-//        }
     }
 
 }
--- src/CloudSkill/Bundle/ManiInPastaBundle/Controller/RegistrazioneController.php
+++ src/CloudSkill/Bundle/ManiInPastaBundle/Controller/RegistrazioneController.php
@@ -13,85 +13,20 @@ use Symfony\Component\Serializer\Exception\Exception;
 class RegistrazioneController extends Controller {
 
     public function registrazioneAction(Request $request, $_format) {
-//        $user = $request->request->get('user');
-//        $pass = $request->request->get('password');
-//        $nome = $request->request->get('nome');
-//        $cognome = $request->request->get('cognome');
-//        $requiredParameters = array("nome","cognome","user","password");
-//        $receivedParameters = $request->request->keys();
-//        
-//        $intersectParams = array_intersect($requiredParameters, $receivedParameters);
-//        if (!count($intersectParams)) {
-//            throw new \Symfony\Component\HttpKernel\Exception\HttpException(400,'You need to specify at least one of ' . implode(',', $requiredParameters));
-//        }
 
-
-        $username = $request->request->get('username');
-        $pass = $request->request->get('password');
-        $nome = $request->request->get('nome');
-        $cognome = $request->request->get('cognome');
-        $email = $request->request->get('email');
-        $status = 'inactive';
-        $token = md5($pass . microtime(true));
-        $tokenDeltaSec = 60 * 60 * 24; //1 giorno
-
-        $timestamp = time();
-        $tokenExpTs = $timestamp + $tokenDeltaSec;
-        $dateTokenExp = new DateTime();
-        $dateTokenExp->setTimestamp($tokenExpTs);
-
-
-
-
-        $user = new User();
-        $user->setUserName($username);
-        $user->setPassword(sha1($pass));
-        $user->setNome($nome);
-        $user->setCognome($cognome);
-        $user->setEmail($email);
-        $user->setStatus($status);
-        $user->setToken($token);
-        $user->setTokenExp($dateTokenExp);
-
-        $em = $this->getDoctrine()->getManager();
-
-        $em->persist($user);
-        $em->flush();
         
         $response = new Response();
-        $response->setContent(json_encode($user->toArray()));
+        $response->setContent(json_encode(array()));
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
 
     public function activateAction(Request $request, $username) {
 
-        
-        $token = $request->query->get('token');
-        if (empty($token)) {
-            throw new \Symfony\Component\HttpKernel\Exception\HttpException(400, "Bad request: token cannot be void");
-        }
-        try {
-            $userRepo = $this->getDoctrine()->getRepository('CloudSkillManiInPastaBundle:User');
-            /* @var $user User */
-            $user = $userRepo->findOneBy(array('username' => $username, 'token' => $token));
-            if ($user == null) {
-                throw new HttpException(403, "Forbidden");
-            }
-            if (!$user->getStatus() or $user->getStatus() != "inactive") {
-                throw new HttpException(422, "Forbidden: user not inactive");
-            }
-             $nowDate=new \Datetime();
-            if ($user->getTokenExp()->getTimestamp() < $nowDate->getTimestamp()) {
-                throw new HttpException(403, "Forbidden: token expired." . $user->getTokenExp()->getTimestamp() . "---" . $nowDate->getTimestamp());
-            }
-            $user->setStatus('active');
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+        try{
             $response = new Response();
             
-            $response->setContent(json_encode($user->toArray()));
+            $response->setContent(json_encode(array()));
             $response->headers->set('Content-Type', 'application/json');
             return $response;
             
